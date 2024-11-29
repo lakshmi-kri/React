099df3bbd91346bfa4d947402d7f3b44d128f262
Harish M B <harishmb@outlook.in>
2024-11-29 12:27:40 +0100

    ReactClass1

---
+++ App.js
@@ -0,0 +1,14 @@
+// const heading = React.createElement("h1", { id: "heading" }, "Hello world from React!");
+// console.log(heading);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement('div', { id: "parent" },
+    [React.createElement("div", { id: "child1" },
+        [React.createElement("h1", {}, "I am h1 Tag"), React.createElement("h2", {}, "I am an h2 tag")]),
+    React.createElement("div", { id: "child2" },
+        [React.createElement("h1", {}, "I am h1 Tag"), React.createElement("h2", {}, "I am an h2 tag")])]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+console.log(parent);    
