9dc2202eddda306b64adff324b06ef24f97ef7da
Lakshmi siddhachar <lakshmiachar93@outlook.com>
2024-12-02 12:47:12 +0100

    checking Config

--- App.js
+++ App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// const heading = React.createElement("h1", { id: "heading" }, "Hello world from React!");
-// console.log(heading);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
+const heading = React.createElement("h1", { id: "heading" }, "Hello world from React!");
+console.log(heading);
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+//root.render(heading);
 
-const parent = React.createElement('div', { id: "parent" },
-    [React.createElement("div", { id: "child1" },
-        [React.createElement("h1", {}, "I am h1 Tag"), React.createElement("h2", {}, "I am an h2 tag")]),
-    React.createElement("div", { id: "child2" },
-        [React.createElement("h1", {}, "I am h1 Tag"), React.createElement("h2", {}, "I am an h2 tag")])]);
 
+const jSXheading = (<h1 id="heading" className="head" tabIndex="5">h1 from JSX</h1>);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log(parent);    
+root.render(jSXheading);
+console.log(jSXheading)
+
