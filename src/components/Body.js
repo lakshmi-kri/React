aa039b286c1d0f6054d3458bb47dcb8ef47338f1
Lakshmi siddhachar <lakshmiachar93@outlook.com>
2024-12-12 09:26:46 +0100

    Episode 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,56 @@
 import RestoCards from "./RestoCards";
 import resList from "../Utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import Shimmer from "./shimmer";
 
 
 const Body = () => {
     //local state variable
-    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
-    return (
+    const [listOfRestaurants, setListOfRestaurants] = useState([]);
+    const [filteredRestaurants, setFilteredRestaurants] = useState([]);
+    const [searchText, setSearchText] = useState("");
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data = await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+        );
+        const json = await data.json();
+        console.log(json);
+        console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
+        //optional chaining
+        setListOfRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+    //conditional rendering
+    // if (listOfRestaurants.length === 0) {
+    //     return <Shimmer />
+    // }
+    // Ternary operator
+    return listOfRestaurants.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
+                <div className="search-bar">
+                    <input type="text"
+                        className="search-box"
+                        value={searchText}
+                        onChange={(e) => { setSearchText(e.target.value) }} />
+                    <button type="button"
+                        onClick={() => {
+                            const filteredlist = listOfRestaurants.filter(
+                                (res) => res.info.name.toLowerCase().includes(searchText.toLowerCase()));
+                            setFilteredRestaurants(filteredlist);
+                            console.log(searchText)
+                        }}>Search</button>
+
+                </div>
                 <button className="filter-btn" onClick={
                     () => {
                         const filteredList = listOfRestaurants.filter(
-                            (res) => (res.data.avgRating > 3)
+                            (res) => (res.info.avgRating > 4.3)
                         )
                         setListOfRestaurants(filteredList)
                     }
@@ -20,8 +58,8 @@ const Body = () => {
                     Top Rated Restaurants</button>
             </div>
             <div className="Res-container">
-                {listOfRestaurants.map((restaurant) => (
-                    <RestoCards key={restaurant.data.id} resData={restaurant} />))
+                {filteredRestaurants.map((restaurant) => (
+                    <RestoCards key={restaurant.info.id} resData={restaurant} />))
                 }
             </div>
         </div>
--- src/components/RestoCards.js
+++ src/components/RestoCards.js
@@ -1,19 +1,21 @@
+import { CDN_URL } from "../Utils/Constants";
+
 const RestoCards = (props) => {
     const { resData } = props;
-    const { name, cuisines, costForTwo, avgRating, deliveryTime } = resData?.data;
+    const { name, cuisines, costForTwo, avgRating, } = resData?.info;
     console.log(props)
     return (
         <div className="res-cards">
-            {/* <img className="res-logo" alt="res-image" src={
-                CDN_CDN_URL +
-                props.resData.data.cloudinaryImageId
-            } /> */}
-            <img className="res-logo" src={resData.data.cloudinaryImageId} />
+            <img className="res-logo" alt="res-image" src={
+                CDN_URL +
+                props.resData.info.cloudinaryImageId
+            } />
+            {/* <img className="res-logo" src={resData.data.cloudinaryImageId} /> */}
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
-            <h4>Rs {costForTwo / 100} for Two</h4>
+            <h4>{costForTwo}</h4>
             <h4>{avgRating} Ratings</h4>
-            <h4>{deliveryTime} Minutes</h4>
+            <h4>{resData.info.sla?.deliveryTime} Minutes</h4>
         </div>
     )
 }
